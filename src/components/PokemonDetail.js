4e12df18682c2c35fd398f4680de24efa1e94e13
Hannes Reinsch <hannes.reinsch99@gmail.com>
2018-08-30 19:34:48 +0200

    added pokemon detail page and pokemon detail card components

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,11 +1,35 @@
 import React from "react";
+import axios from "axios";
+import Loading from "./Loading";
+import PokemonDetailCard from "./PokemonDetailCard";
 
-const PokemonDetail = () => {
-  return (
-    <React.Fragment>
-      <h2>PokemonDetail</h2>
-    </React.Fragment>
-  );
-};
+class PokemonDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.id}`)
+      .then(pokemon => {
+        this.setState({
+          pokemon: [...this.state.pokemon, pokemon.data]
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>PokemonDetail</h2>
+        {this.state.pokemon.length < 1 ? <Loading /> : <PokemonDetailCard />}
+      </React.Fragment>
+    );
+  }
+}
 
 export default PokemonDetail;
